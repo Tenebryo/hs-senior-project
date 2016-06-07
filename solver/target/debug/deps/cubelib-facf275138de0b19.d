960dd3a2b5b5eaedb7832322984cfbc49f4864d1
Sam Blazes <sblazes16@gmail.com>
2016-06-07 10:01:13 -0400

    removed unused code that has been moved elsewhere

--- solver/target/debug/deps/cubelib-facf275138de0b19.d
+++
@@ -1,6 +0,0 @@
-D:\dev\Other\hs-senior-project\solver\target\debug\deps\cubelib-facf275138de0b19.d: D:\dev\Other\hs-senior-project\cubelib\src/lib.rs D:\dev\Other\hs-senior-project\cubelib\src\cube.rs
-
-D:\dev\Other\hs-senior-project\solver\target\debug\deps\libcubelib-facf275138de0b19.rlib: D:\dev\Other\hs-senior-project\cubelib\src/lib.rs D:\dev\Other\hs-senior-project\cubelib\src\cube.rs
-
-D:\dev\Other\hs-senior-project\cubelib\src/lib.rs:
-D:\dev\Other\hs-senior-project\cubelib\src\cube.rs:
